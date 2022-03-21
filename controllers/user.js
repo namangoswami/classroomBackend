a73fda233c48960d57824dc210afe5b219a8992a
Naman Goswami <namansuman01@gmail.com>
2022-03-21 08:11:19 +0530

    Fix User in array issue

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,8 @@ var User=require('../models/users.js');
 
 module.exports.getUserClasses=(req, res)=>{
     const user=req.user;
+    const userProps={email:user.email, firstName:user.firstName, lastName:user.lastName}
+    // console.log(user);
     Class.find({$or:[{students:{$in:[user]}}, {teacher:user}]})
     .exec((err, classObjAr)=>{
         if(err){
